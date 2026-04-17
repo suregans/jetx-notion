fafc2378b443829ae4c74af5a214f1012f59beee
Suregan <suregan.s@gmail.com>
2026-04-17 06:57:39 -0400

    v4.5 - Auto-schema detection, field toggles, graph view, branding to WP Admin, PHP version gate

--- push-to-github.ps1
+++ push-to-github.ps1
@@ -1,3 +1,5 @@
 git add .
-git commit -m "v4.0.0 - Auto-schema detection, field toggles, graph view, branding to WP Admin, PHP version gate"
+git commit -m "v4.5 - Auto-schema detection, field toggles, graph view, branding to WP Admin, PHP version gate"
+git tag v4.5
 git push origin main
+git push origin v4.5
